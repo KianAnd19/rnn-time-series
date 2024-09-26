cf50f74fd4d056e533b152615faa2da6d305e9c7
Kian Anderson <24872849@sun.ac.za>
2024-09-26 23:42:48 +0200

    added rmse among other things, just need to figure out how to k-fold properly

--- hand/main.py
+++ hand/main.py
@@ -9,7 +9,7 @@ from utils import linear_detrend
 rnns = [ElmanRNN, JordanRNN, MultiRNN]
 datasets = ['air_passengers', 'electric_production', 'minimum_temp', 'beer_production', 'gold_price', 'yahoo_stock']
 
-def preprocess_data(filename, sequence_length=10):
+def preprocess_data(filename, sequence_length=10, count=0):
     with open(filename, "r") as f:
         lines = f.readlines()[1:]  # Skip header
         data = []
@@ -19,7 +19,9 @@ def preprocess_data(filename, sequence_length=10):
             data.append([date.timestamp(), float(production)])
     
     data = np.array(data)
-    # data[:, 1] = linear_detrend(data[:, 1])
+    if(count < 4):
+        print('detrend')
+        data[:, 1] = linear_detrend(data[:, 1])
 
     # Normalize the data
     scaler = MinMaxScaler()
@@ -47,8 +49,32 @@ def train_test(rnn, X_train, X_test, Y_train, Y_test, epochs):
 
     # Calculate Mean Absolute Error
     mae = np.mean(np.abs(Y_test_original-predictions_original))
+    mase = mae/((1/(len(predictions_original)-1)))
+    rmse = np.sqrt(np.mean(np.power(Y_test_original-predictions_original, 2))/len(predictions_original)) 
     
-    return mae
+    return rmse
+
+def cv_validation(rnn, X, Y, scaler):
+    size_split = int((len(X) / k)*0.8)
+    
+    avg_result = 0
+
+    for i in range(k-1, -1, -1):
+        total_sample = round(1 - ((1/k)*i), 2)
+        split = int(0.8 * total_sample * len(X))
+        total_sample = int(total_sample * len(X))
+    
+        X_train, X_test = X[split-size_split:split], X[split:total_sample]
+        Y_train, Y_test = Y[split-size_split:split], Y[split:total_sample]
+    
+        result = train_test(rnn, X_train, X_test, Y_train, Y_test, epochs)
+        avg_result += result 
+
+        print(total_sample, '\t', result)
+
+    avg_result /= k
+    print('Average Result: ', avg_result)
+
 
 ######################################################
 #################### main ############################
@@ -58,34 +84,25 @@ def train_test(rnn, X_train, X_test, Y_train, Y_test, epochs):
 input_size = 15  # sequence length
 hidden_size = 10
 output_size = 1
-learning_rate = 1e-4
+learning_rate = 1e-3
 k = 5 # number of folds for k-fold cross validation
 epochs = 1000
 
+
+hyperparameters = {
+    'lr': [1e-2, 1e-3, 1e-4, 1e-5],
+    'hidden_size': [5, 10, 15],
+    'input_size': [5, 10, 15],
+    'epochs': [500, 1000, 1500]
+}
+
 for rnn in rnns:
-    for i in range(5):
+    for i in range(6):
         print('Dataset: ', datasets[i])
         # Preprocess the data
-        X, Y, scaler = preprocess_data(f'new_datasets/{datasets[i]}.csv', sequence_length=input_size)
+        X, Y, scaler = preprocess_data(f'new_datasets/{datasets[i]}.csv', sequence_length=input_size, count=i)
         
-        size_split = int((len(X) / k)*0.8)
-        
-        avg_result = 0
+        r = rnn(input_size, hidden_size, output_size, learning_rate=learning_rate)
+        cv_validation(r, X, Y, scaler)
 
-        for i in range(k-1, -1, -1):
-            rnn = rnns[0](input_size, hidden_size, output_size, learning_rate=learning_rate)
-            total_sample = round(1 - ((1/k)*i), 2)
-            split = int(0.8 * total_sample * len(X))
-            total_sample = int(total_sample * len(X))
-        
-            X_train, X_test = X[split-size_split:split], X[split:total_sample]
-            Y_train, Y_test = Y[split-size_split:split], Y[split:total_sample]
-        
-            result = train_test(rnn, X_train, X_test, Y_train, Y_test, epochs)
-            avg_result += result 
-
-            print(total_sample, '\t', result)
-
-        avg_result /= k
-        print('Average Result: ', avg_result)
     break
